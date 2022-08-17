9834e03bdfc85c07bd46a57fd7c2ed8fea96927f
surikbs <surikbs@gmail.com>
2022-08-17 09:25:01 +0530

    changes

--- statusall.sh
+++ statusall.sh
@@ -1,7 +1,4 @@
 ansible-playbook -i hosts.yml manageservices/zookeeper/zk_status.yaml
-sleep 10
 ansible-playbook -i hosts.yml manageservices/kafka/kafka_broker_status.yaml
-sleep 10
 ansible-playbook -i hosts.yml manageservices/kafka_connect/kafka_connect_status.yaml
-sleep 10
 ansible-playbook -i hosts.yml manageservices/control-center/kafka_ccc_status.yaml
